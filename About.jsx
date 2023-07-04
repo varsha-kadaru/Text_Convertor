7660c9d491598cfb85016df67407f2c41eb7c600
varsha-kadaru <107875699+varsha-kadaru@users.noreply.github.com>
2023-07-04 16:05:48 +0530

    Update About.jsx

---
+++ About.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+export default function About() {
+  return (
+    <div>
+      <p>
+        Easy tool to transform your text
+        Uppercase, lowercase, Random lorem ipsum text, Speech word Count, Word Counter, Extract email from text and many more.
+      </p>
+    </div>
+
+  )
+}
